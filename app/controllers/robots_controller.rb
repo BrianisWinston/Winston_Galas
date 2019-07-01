f74c37d13bf23641f569d5c7f37a6fbecc0f3490
Winston Galas <BrianisWinston@yahoo.com>
2019-06-30 23:35:34 -0700

    Finish generating robot flow

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,7 +1,4 @@
 class RobotsController < ApplicationController
-  require 'net/http'
-  require 'uri'
-
   def show
     if params[:robot_string] == nil || params[:robot_string].empty?
       @string = nil
